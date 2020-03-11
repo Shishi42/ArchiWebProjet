0c883a2c5f649031e1a7f93096fd99ec60d4a7c4
Riviere Benjamin <s172032@ic2s114-10.univ-lemans.fr>
2020-03-11 10:39:55 +0100

    On est content

--- README.md
+++ README.md
@@ -3,6 +3,6 @@
   </head>
   <body>
     <h1>Archi Web</h1>
-    <h3>Projet de L3 Architecture Web</h3>
+    <h3>Projet de L3 INFO - Architecture Web</h3>
   </body>
 </html>
--- dashboard/src/app/layouts/admin-layout/admin-layout.module.ts
+++ dashboard/src/app/layouts/admin-layout/admin-layout.module.ts
@@ -6,7 +6,6 @@ import { AdminLayoutRoutes } from './admin-layout.routing';
 import { DashboardComponent } from '../../dashboard/dashboard.component';
 import { TableListComponent } from '../../table-list/table-list.component';
 import { TypographyComponent } from '../../typography/typography.component';
-import { IconsComponent } from '../../icons/icons.component';
 import { MapsComponent } from '../../maps/maps.component';
 
 import {
@@ -34,7 +33,6 @@ import {
     DashboardComponent,
     TableListComponent,
     TypographyComponent,
-    IconsComponent,
     MapsComponent,
   ]
 })
