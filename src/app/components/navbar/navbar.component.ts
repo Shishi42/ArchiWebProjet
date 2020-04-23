23b16469fcc5d3a4e008dd4075606f96bee30984
lepoittevin <eliot.lepoittevin@gmail.com>
2020-04-24 00:36:15 +0200

    ajout BD+Barre de recherche fonctionelle+requette API kitsu asynchrone+actualisation affichage site

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -2,26 +2,53 @@ import { Component, OnInit, ElementRef } from '@angular/core';
 import { ROUTES } from '../sidebar/sidebar.component';
 import {Location, LocationStrategy, PathLocationStrategy} from '@angular/common';
 import { Router } from '@angular/router';
+import { FormBuilder, FormGroup } from '@angular/forms';
+//import { inflate } from 'zlib';
+
+class Info {
+    synopsis: string
+    updateAt: string
+    nbEpisode: string
+    coverImage: string
+    nom: string
+    image: string
+    rating: string
+}
+
+
+var counterAsync=0;
+
+
+var infosBD: Array<Info>;
+infosBD=new Array<Info>();
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+  styleUrls: ['./navbar.component.css'],
 })
+
+
+
 export class NavbarComponent implements OnInit {
     private listTitles: any[];
     location: Location;
-      mobile_menu_visible: any = 0;
+    mobile_menu_visible: any = 0;
     private toggleButton: any;
     private sidebarVisible: boolean;
 
+
+
+    protected champRecherche: String;
+
     constructor(location: Location,  private element: ElementRef, private router: Router) {
-      this.location = location;
-          this.sidebarVisible = false;
+        this.location = location;
+        this.sidebarVisible = false;
+        this.champRecherche="";
     }
 
     ngOnInit(){
-      this.listTitles = ROUTES.filter(listTitle => listTitle);
+      /*this.listTitles = ROUTES.filter(listTitle => listTitle);
       const navbar: HTMLElement = this.element.nativeElement;
       this.toggleButton = navbar.getElementsByClassName('navbar-toggler')[0];
       this.router.events.subscribe((event) => {
@@ -31,11 +58,12 @@ export class NavbarComponent implements OnInit {
            $layer.remove();
            this.mobile_menu_visible = 0;
          }
-     });
+     });*/
+     
     }
 
     sidebarOpen() {
-        const toggleButton = this.toggleButton;
+        /*const toggleButton = this.toggleButton;
         const body = document.getElementsByTagName('body')[0];
         setTimeout(function(){
             toggleButton.classList.add('toggled');
@@ -43,18 +71,18 @@ export class NavbarComponent implements OnInit {
 
         body.classList.add('nav-open');
 
-        this.sidebarVisible = true;
+        this.sidebarVisible = true;*/
     };
     sidebarClose() {
-        const body = document.getElementsByTagName('body')[0];
+        /*const body = document.getElementsByTagName('body')[0];
         this.toggleButton.classList.remove('toggled');
         this.sidebarVisible = false;
-        body.classList.remove('nav-open');
+        body.classList.remove('nav-open');*/
     };
     sidebarToggle() {
         // const toggleButton = this.toggleButton;
         // const body = document.getElementsByTagName('body')[0];
-        var $toggle = document.getElementsByClassName('navbar-toggler')[0];
+        /*var $toggle = document.getElementsByClassName('navbar-toggler')[0];
 
         if (this.sidebarVisible === false) {
             this.sidebarOpen();
@@ -106,11 +134,11 @@ export class NavbarComponent implements OnInit {
             body.classList.add('nav-open');
             this.mobile_menu_visible = 1;
 
-        }
+        }*/
     };
 
     getTitle(){
-      var titlee = this.location.prepareExternalUrl(this.location.path());
+      /*var titlee = this.location.prepareExternalUrl(this.location.path());
       if(titlee.charAt(0) === '#'){
           titlee = titlee.slice( 1 );
       }
@@ -120,6 +148,159 @@ export class NavbarComponent implements OnInit {
               return this.listTitles[item].title;
           }
       }
-      return 'Dashboard';
+      return 'Dashboard';*/
+    }
+
+    
+    //methode appelé lors du clic sur le bouton recherche
+    cherche(){
+        let valBarreRecherche = (<HTMLInputElement> document.getElementById("barreRecherche")).value;
+        alert("recherche Avec :"+valBarreRecherche);
+        actuInfos(valBarreRecherche)
+    }
+    
+    
+
+}
+
+
+
+
+
+//gere les actualisation de la page en fonction de la recherche effectué
+function actuInfos(nom: String){
+    //test si le nom est deja stocké dans la base de données
+    var indiceInfoBD=estPresentBD(nom);
+    if(indiceInfoBD!=-1){ 
+
+        console.log("AFFICHAGE DIRECT CAR DEJA INFO BD");
+        afficherInfo( infosBD[indiceInfoBD] );
+
+    }else{
+
+        console.log("ACCES API CAR PAS D'INFO BD");
+        ActuWithAPI(nom);
+
+    }
+
+}
+
+
+function estPresentBD(nom: String):number{
+    for( var indice=0 ; indice < (infosBD.length-1) ; indice++ ){
+        if(infosBD[indice].nom==nom){
+            return indice;
+        }
+    }
+    return -1;
+}
+
+
+
+
+//actualise tt les infos de la page avec la case de la BD entré en param
+function afficherInfo(info: Info){
+
+    (<HTMLInputElement> document.getElementById("infoTitre")).innerHTML=info.nom;
+
+    (<HTMLInputElement> document.getElementById("infoImage")).src=info.image;
+
+    (<HTMLInputElement> document.getElementById("infoRating")).innerHTML="<b>Rating : </b>"+info.rating+" / 100";
+
+    (<HTMLInputElement> document.getElementById("infoEpisode")).innerHTML="<b>Episodes : </b>"+info.nbEpisode;
+
+    (<HTMLInputElement> document.getElementById("infoSynopsis")).innerHTML="<b>Synopsis : </b>"+info.synopsis;
+
+    (<HTMLInputElement> document.getElementById("infoUpdateAt")).innerHTML="<b>Mis à jour le </b>"+info.updateAt;
+
+
+}
+
+
+//recuperes tt les info dans les différentes API et les stock dans un champ de la BD
+function ActuWithAPI(nom: String){
+    counterAsync=0;
+    //creation d'un champ que l'on va push dans la BD apres l'avoir complété grace aux requettes
+    var info=new Info();
+
+    //appel des API et remplissage au fur et a mesure de du champ.
+    APIKitsu(nom,info);
+    APITwitter(nom,info);
+
+    
+}
+
+
+
+
+
+function APIKitsu(name :String,info: Info){
+
+    $.ajax({
+
+        url: "https://kitsu.io/api/edge/anime",
+
+        data: {"filter[text]" : name},
+
+        type: 'get',
+
+        dataType: 'json',
+
+        async: true,
+
+        success: function(json) {
+
+            if(json.data.length>0){
+                var attrs=json.data[0].attributes;
+
+                info.nom=attrs.titles.en;
+                info.synopsis=attrs.synopsis;
+                info.updateAt=attrs.updatedAt;
+                info.image=attrs.coverImage.tiny;
+                info.nbEpisode=attrs.episodeCount;
+                info.rating=attrs.averageRating;
+
+
+                console.log("kitsu"+counterAsync);
+                callBackFonct(info);
+
+            }else{
+                console.log(json.data);
+                alert("Aucune info dans la BD ni dans l'API Kitsu pour "+name);
+            }
+
+        },
+
+        error: function (xhr, status, error) {
+
+            alert('Erreur HTTP '+xhr.responseText);
+
+        }
+
+    });
+
+}
+
+
+
+function APITwitter(nom :String,info: Info){
+    //envois requette et recup info
+
+    console.log("twitter"+counterAsync);
+    callBackFonct(info);
+
+
+}
+
+
+function callBackFonct(info: Info){
+    counterAsync++;
+    if(counterAsync==2){
+        infosBD.push(info);
+        afficherInfo( info );
+
+        console.log(infosBD);
     }
 }
+
+
